e0df5160e16cdbd4c8c31b9ddaa4bac509c0239a
Chandra Shekhar <Shekhar88085@gmail.com>
2020-12-24 19:06:24 +0530

    placement time

---
+++ src/Placement/LogestConsecutiveSubsequence.java
@@ -0,0 +1,14 @@
+package Placement;
+
+import java.sql.Array;
+import java.util.Arrays;
+
+public class LogestConsecutiveSubsequence {
+    static  int FindLCS(int arr[], int n){
+        Arrays.sort(arr);
+
+    }
+    public static void main(String [] dsaaa){
+
+    }
+}
